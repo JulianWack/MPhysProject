16d7510f16bcf05a3a4d28776e107ed7237ff6a3
Julian <j.f.wack@sms.ed.ac.uk>
2022-11-28 18:38:21 +0000

    extend routines to work on a lattice of matrices rather than individual ones

--- SU2xSU2/SU2_mat_routines.py
+++ SU2xSU2/SU2_mat_routines.py
@@ -3,6 +3,8 @@
 # Specifically, an SU(2) matrix is fully specified by 4 real parameters:
 # [[a0 + i*a3, a2 + i*a1],
 # [-a2 + i*a1, a0 - i*a3]]
+#
+# The operations are designed to be applies to a N by N lattice of SU(2) matrices, i.e. to parameter array of shape (N,N,4).
 
 
 import numpy as np
@@ -12,45 +14,75 @@ def alpha_to_a(alpha):
     '''Convert parameters of SU(2) matrix when given in terms of the exponential map to the parameters when given as linear combination of gens and unity:
     U = exp(i alpha_i sigma_i) = a_0*1 + i*a_i sigma_i
         
-    alpha: 1D array length 3
-        parameters when representing a SU(2) group element via the exponential map
+    alpha: (N,N,3) array
+        parameters when representing a SU(2) group element via the exponential map at every lattice site
 
     Returns:
-    a: 1D array length 4
-        parameters when explicitly evaluating the exponential map
+    a: (N,N,4) array
+        parameters of matrices at each lattice site when explicitly evaluating the exponential map
     '''
-    a = np.empty(4)
-    alpha_norm = np.sqrt(np.sum(alpha))
-    alpha_unitvec = alpha / alpha_norm
-    a[0] = np.cos(alpha_norm)
-    a[1:] = alpha_unitvec * np.sin(alpha_norm)
+    N = alpha.shape[0]
+    a = np.empty((N,N,4))
+    norm = np.sqrt(np.sum(alpha, axis=2)) # (N,N)
+    # to do arithmetic with other (N,N,3) array need to broadcast to include axis 2
+    alpha_norm = norm.reshape((N,N,1)) # more basic approach would be np.stack([alpha_norm, alpha_norm, alpha_norm], axis=2) but required 3 times as much memory
+    alpha_unit = alpha / alpha_norm  
+    a[:,:,0] = np.cos(norm)
+    a[:,:,1:] = alpha_unit * np.sin(alpha_norm)
 
     return a
 
 
-def make_mat(a):
-    '''Constructs full matrix explicitly.
-    '''
-    mat = [[a[0]+1j*a[3], a[2]+1j*a[1]], 
-    [-a[2]+1j*a[1], a[0]-1j*a[3]]]
+def make_mats(a):
+    '''Constructs explicit matrices corresponding to parameter vectors a.
 
-    return np.matrix(mat)
+    a: (N,N,4) array
+        parameters of the SU(2) matrices
+
+    Returns:
+    mats: (N,N) object array
+        np.matrix instance at every site
+    '''
+    N = a.shape[0]
+    mats = np.empty((N,N), dtype=object)
+
+    for i in range(N):
+        for j in range(N):
+            paras = a[i,j,:]
+            mat = [[paras[0]+1j*paras[3], paras[2]+1j*paras[1]], 
+                [-paras[2]+1j*paras[1], paras[0]-1j*paras[3]]]
+            mats[i,j] = np.matrix(mat)
+
+    return mats
 
 
 ### basic quantities ###
 def hc(a):
-    '''Returns parameter vector of the hermitian conjugate.
-    '''
-    new_a = -a 
-    new_a[0] = a[0]
+    '''Returns the parameter vector of the hermitian conjugate at each lattice site.
 
-    return new_a
+    a: (N,N,4) array
+        parameters of matrices
+
+    Returns:
+    a_hc: (N,N,4) array
+        parameters of hermitian conjugate matrices
+    '''
+    a_hc = -a 
+    a_hc[:,:,0] = a[:,:,0]
+
+    return a_hc
 
 
 def tr(a):
-    '''Returns trace
+    '''Returns trace of matrices at each lattice site
+
+    a: (N,N,4) array
+        parameters of matrices
+
+    Returns: (N,N) array trace at each site 
     '''
-    return 2*a[0]
+    trace = 2*a[:,:,0]
+    return trace
 
 
 def det(a):
@@ -61,41 +93,66 @@ def det(a):
 
 def norm2(a):
     '''Returns squared norm of parameter vector
+    
+    a: (N,N,4) array
+        parameters of matrices
+
+    Returns: (N,N) array containing the norm at each site
     '''
-    return np.sum(a**2)
+    return np.sum(a**2, axis=2)
 
 
 def renorm(a):
     '''Renormalises matrix to have det = 1
     '''
-    return a / np.sqrt(norm2(a))
+    N = a.shape[0]
+    norm =  np.sqrt(norm2(a)).reshape((N,N,1)) # broadcast to do arithmetic with (N,N,4) array a
+    
+    return a / norm
 
 
 ### combining two SU(2) matrices ### 
 def dot(a, b):
     '''Computes matrix product A.B when matrices A and B have associated parameter vectors a and b.
+
+    a,b: (N,N,4) array
+        parameters of matrices at each lattice site
+
+    Returns:
+    c: (N,N,4) array
+        parameters of matrix products   
     '''
-    c0 = a[0]*b[0] - np.sum(a[1:]*b[1:])
-    c1 = a[0]*b[1] + a[1]*b[0] + a[3]*b[2] - a[2]*b[3]
-    c2 = a[0]*b[2] + a[2]*b[0] + a[1]*b[3] - a[3]*b[1]
-    c3 = a[0]*b[3] + a[3]*b[0] + a[2]*b[1] - a[1]*b[2]
-    c = np.array([c0, c1, c2, c3])
+    c = np.empty_like(a)
+    c[:,:,0] = a[:,:,0]*b[:,:,0] - np.sum(a[:,:,1:]*b[:,:,1:], axis=2)
+    c[:,:,1] = a[:,:,0]*b[:,:,1] + a[:,:,1]*b[:,:,0] + a[:,:,3]*b[:,:,2] - a[:,:,2]*b[:,:,3]
+    c[:,:,2] = a[:,:,0]*b[:,:,2] + a[:,:,2]*b[:,:,0] + a[:,:,1]*b[:,:,3] - a[:,:,3]*b[:,:,1]
+    c[:,:,3] = a[:,:,0]*b[:,:,3] + a[:,:,3]*b[:,:,0] + a[:,:,2]*b[:,:,1] - a[:,:,1]*b[:,:,2]
 
     return c
 
 
 def sum(a, b):
-    '''Computes sum of two SU(2) matrices A and B with associated parameter vectors a and b.
+    '''Computes sum of two SU(2) matrix lattices A and B with parameters a and b.
     Let C = A + B, i.e. c = a + b. 
     Note that the sum of two SU(2) matrices is proportional to an SU(2) matrix with proportionality constant k, meaning
     D = C/k = 1/k (A + B) is in SU(2).
-    To only having to perform manipulations on SU(2) matrices, the parameters d of the SU(2) matrix D and the constant k is returned such that their product gives the 
-    parameter vector of C, the sum of A and B.
+    To only having to perform manipulations on SU(2) matrices, the parameters d of the SU(2) matrix lattice D and the constant k is returned such that their product gives the 
+    parameter vectors of C, the sum of lattice A and B.
+
+    a,b: (N,N,4) array
+        parameters of matrices to sum at each lattice site
+
+    Returns:
+    d: (N,N,4) array
+        parameters of SU(2) matrices
+    k: (N,N,1) array
+        proportionality constant between d and sum of matrices
+
     '''
     c = a + b
-    k_2 = 2*(a[0]*b[0] + a[1:]*b[1:] + 1)
-
-    k = np.sqrt(k_2)
+    k2 = 2*(a[:,:,0]*b[:,:,0] + np.sum(a[:,:,1:]*b[:,:,1:], axis=2) + 1) # (N,N)
+    N = a.shape[0]
+    k = np.sqrt(k2, dtype=complex).reshape((N,N,1))
     d = c / k
 
     return d, k
--- SU2xSU2/tests.py
+++ SU2xSU2/tests.py
@@ -4,32 +4,101 @@ import SU2_mat_routines as SU2
 
 
 ##### Check if my matrix routine gives same result as np #####
-def test_SU2_mat():
+def test_SU2_routines():
+    # Define a N by N lattice and and describe matrices at every site by some arbitrary parameters
+    N = 10
     # np.random.seed(42)
-    aa = 10*np.random.random(4)
-    bb = -66.8*np.random.random(4)
+    aa = 10*np.random.random((N,N,4))
+    bb = -66.8*np.random.random((N,N,4))
 
+    # compute quantities to compare using my routines
     my_A = aa
     my_B = bb
+    my_C = SU2.dot(my_A, my_B)
 
+    # compute quantities to compare using np routines
     def make_np_mat(a):
         return np.matrix( [[a[0]+1j*a[3], a[2]+1j*a[1]], [-a[2]+1j*a[1], a[0]-1j*a[3]]] )
 
-    np_A = make_np_mat(aa)
-    np_B = make_np_mat(bb)
+    np_A = np.empty((N,N), dtype=object)
+    np_B = np.empty((N,N), dtype=object)
+    np_C = np.empty((N,N), dtype=object)
 
-    my_C = SU2.make_mat( SU2.dot(my_A, my_B) )
-    np_C = np.matmul(np_A, np_B)
-
-    # need to convert np.matrix object into ndarray (via .A) for allclose comparison 
-    print('Same matrix product: ', np.allclose(my_C.A, np_C.A))
-    my_hc = SU2.make_mat(SU2.hc(my_A))
-    print('Same hc: ', np.allclose(my_hc.A, np_A.H.A))
-    print('Same det: ', np.allclose(SU2.det(my_A), np.linalg.det(np_A.A).real))
-    print('Same trace: ', np.allclose(SU2.tr(my_A), np.trace(np_A.A).real))
+    for i in range(N):
+        for j in range(N):
+            np_A[i,j] = make_np_mat(aa[i,j,:])
+            np_B[i,j] = make_np_mat(bb[i,j,:])
+            np_C[i,j] = np.matmul(np_A[i,j], np_B[i,j])
 
 
-    ##### Compare speed of matrix multiplication #####
+    # compare results: need to convert np.matrix object into ndarray (via .A) for allclose comparison 
+    # sum 
+    su2_element, k = SU2.sum(my_A, my_B)
+    my_sum = SU2.make_mats(k*su2_element)
+    all_equal = True
+    for i in range(N):
+        for j in range(N):
+            same = np.allclose(my_sum[i,j].A, (np_A[i,j]+np_B[i,j]).A)
+            if not same:
+                all_equal = False
+                print('Unequal sum at site: (%d, %d)'%(i,j))
+    if all_equal:
+        print('All sums equal')
+
+    # product
+    my_prod = SU2.make_mats(my_C)
+    all_equal = True
+    for i in range(N):
+        for j in range(N):
+            same = np.allclose(my_prod[i,j], np_C[i,j])
+            if not same:
+                all_equal = False
+                print('Unequal product at site: (%d, %d)'%(i,j))
+    if all_equal:
+        print('All products equal')
+
+    # hermitian conjugate
+    my_hc = SU2.make_mats(SU2.hc(my_A))
+    all_equal = True
+    for i in range(N):
+        for j in range(N):
+            same = np.allclose(my_hc[i,j].A, (np_A[i,j].H).A)
+            if not same:
+                all_equal = False
+                print('Unequal hc at site: (%d, %d)'%(i,j))
+    if all_equal:
+        print('All hc equal')
+
+    # det
+    my_det = SU2.det(my_A)
+    all_equal = True
+    for i in range(N):
+        for j in range(N):
+            same = np.allclose(my_det[i,j], np.linalg.det(np_A[i,j].A).real)
+            if not same:
+                all_equal = False
+                print('Unequal det at site: (%d, %d)'%(i,j))
+    if all_equal:
+        print('All det equal')
+
+    # trace
+    my_tr = SU2.tr(my_A)
+    all_equal = True
+    for i in range(N):
+        for j in range(N):
+            same = np.allclose(my_tr[i,j], np.trace(np_A[i,j].A).real)
+            if not same:
+                all_equal = False
+                print('Unequal tr at site: (%d, %d)'%(i,j))
+    if all_equal:
+        print('All tr equal')
+
+# test_SU2_routines()
+########
+
+
+##### Compare speed of single matrix multiplication #####
+def test_SU2_prod_speed():
     # tabbing important to assure that string has no tabs. Otherwise timeit throws an error
     # when using timeit outside a function, can tab all lines to the same height
 
@@ -39,8 +108,8 @@ import SU2_mat_routines as SU2
 '''
 
     my_test_code = ''' 
-aa = np.random.random(4)
-bb = np.random.random(4)
+aa = np.random.random((1,1,4))
+bb = np.random.random((1,1,4))
 
 my_A = aa
 my_B = bb
@@ -61,10 +130,9 @@ np_B = make_np_mat(bb)
 np.matmul(np_A, np_B)
 '''
 
-    n_iter = 100000
+    n_iter = 10000
     # print total time needed to perform n_iter executions of the test code
     print('My product: ', timeit.timeit(setup=set_up, stmt=my_test_code, number=n_iter))
     print('np product: ', timeit.timeit(setup=set_up, stmt=np_test_code, number=n_iter))
 
-test_SU2_mat()
-########
+# test_SU2_prod_speed()
