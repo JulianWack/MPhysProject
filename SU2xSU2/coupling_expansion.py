867e56c6f809141cd3404431a56c532a06cc7af7
Julian <j.f.wack@sms.ed.ac.uk>
2022-12-30 14:27:30 +0000

    refactor ell, eps calibration

--- SU2xSU2/coupling_expansion.py
+++ SU2xSU2/coupling_expansion.py
@@ -6,48 +6,12 @@ import time
 from datetime import timedelta
 
 from SU2xSU2 import SU2xSU2
+from calibrate_paras import calibrate
 
 plt.style.use('science')
 plt.rcParams.update({'font.size': 20})
 # plt.rcParams.update({'text.usetex': False}) # faster rendering
 mpl.rcParams['axes.prop_cycle'] = cycler(color=['k', 'g', 'b', 'r'])
-
-
-def run_sims(model_paras, sim_paras):
-    '''Performs simulation specified by model and simulation parameters.
-    The value pair eps and ell, yielding a desired acceptance rate between 60% and 80%, depends on the simulation parameters.
-    One needs to run a full simulation to test if the considered values of eps and ell are suitable. If not, the step size is changed according to the difference to the ideal
-    acceptance rate of 65%. The number of steps is then constraint by requiring a unit trajectory length. 
-    Manual inspection of the acceptance rate might be necessary as the search for eps, ell is stopped after 10 iterations to avoid the program to get caught in a loop.
-
-    For a new set of simulation parameters, it is best to run short simulations (~100) first to get sensible values of eps, ell. These can then be used as guesses in production runs.
-
-    Returns
-    ell, eps, acc_rate: floats
-        inferred number of steps and step size and corresponding acceptance rate
-    s_avg, s_err, IAT_s, IAT_s_err: floats
-        values of the measurements taken
-    '''
-    good_acc_rate = False
-    count = 0 
-    while good_acc_rate == False:
-        model = SU2xSU2(**model_paras)
-        model.run_HMC(**sim_paras)  
-        acc_rate = model.acc_rate
-        d_acc_rate = 0.65 - acc_rate
-        if count >= 10:
-            good_acc_rate = True
-        if acc_rate < 0.6 or acc_rate > 0.8:
-            model_paras['ell'] = int(np.rint(model_paras['ell']*(1 + d_acc_rate)))
-            model_paras['eps'] = 1/model_paras['ell']
-            count +=1
-        else:
-            good_acc_rate = True
-    ell, eps = model_paras['ell'], model_paras['eps']
-    # make measurements
-    e_avg, e_err, IAT_e, IAT_e_err = model.internal_energy_density()
-
-    return ell, eps, acc_rate, e_avg, e_err, IAT_e, IAT_e_err
 
 
 def internal_energy_coupling_exp(betas, save=False):
@@ -66,18 +30,19 @@ def internal_energy_coupling_exp(betas, save=False):
     # load previous guesses of eps, ell for different beta
     eps_guess = np.loadtxt('data/coupling_expansion')[:,2]
     ell_guess = np.loadtxt('data/coupling_expansion')[:,1]
-    # guesses to calibrate simulations
-    # eps_guess = np.full_like(betas, 0.1)
-    # ell_guess = np.full_like(betas, 10)
+    # use default guesses when no results are stored
+    # eps_guess = np.full_like(betas, 1/7)
+    # ell_guess = np.full_like(betas, 7)
 
     t1 = time.time()
     for i, beta in enumerate(betas):
         model_paras['ell'] = int(ell_guess[i])
         model_paras['eps'] = eps_guess[i]
         model_paras['beta'] = beta
-        # ell, eps, acc_rate, e_avg, e_err, IAT_e, IAT_e_err = run_sims(list(model_paras.values()), list(sim_paras.values()))
-        ell, eps, acc_rate, e_avg, e_err, IAT_e, IAT_e_err = run_sims(model_paras, sim_paras)
-        data[i] = np.array([beta, ell, eps, acc_rate, e_avg, e_err, IAT_e, IAT_e_err])
+        # model_paras = calibrate(model_paras) # uncomment for preliminary calibration when using default guesses
+        model, paras = calibrate(model_paras, sim_paras, production_run=True)
+        e_avg, e_err, IAT_e, IAT_e_err = model.internal_energy_density()
+        data[i] = np.array([beta, paras['ell'], paras['eps'], model.acc_rate, e_avg, e_err, IAT_e, IAT_e_err])
         print('-'*32)
         print('Completed %d / %d: beta=%.3f'%(i+1, len(betas), beta))
         print('-'*32)
@@ -110,7 +75,7 @@ def internal_energy_coupling_exp(betas, save=False):
     plt.legend(prop={'size': 12})
 
     plt.show()
-    fig.savefig('plots/coupling_exp.pdf')
+    # fig.savefig('plots/coupling_exp.pdf')
 
 
     return 
--- SU2xSU2/find_thinning_freq.py
+++ SU2xSU2/find_thinning_freq.py
@@ -4,6 +4,7 @@ import matplotlib as mpl
 from cycler import cycler
 
 from SU2xSU2 import SU2xSU2
+from calibrate_paras import calibrate
 
 
 plt.style.use('science')
@@ -12,39 +13,39 @@ plt.rcParams.update({'font.size': 20})
 mpl.rcParams['axes.prop_cycle'] = cycler(color=['k', 'g', 'b', 'r'])
 
 thin_freqs = np.logspace(np.log10(2), 2, 10, dtype=int)
-s_IAT = np.zeros_like(thin_freqs, dtype=float)
-s_IAT_err = np.zeros_like(thin_freqs, dtype=float)
-c_IAT = np.zeros_like(thin_freqs, dtype=float)
-c_IAT_err = np.zeros_like(thin_freqs, dtype=float)
+e_IAT = np.zeros_like(thin_freqs, dtype=float)
+e_IAT_err = np.zeros_like(thin_freqs, dtype=float)
 
-model = SU2xSU2(N=16, a=1, ell=10, eps=0.1, beta=1)
-# # runs ingle simulation and store unthinned chain 
-# model.run_HMC(20000, 1, 0.1, store_data=True) 
-# # or load stored simulation
-paras = np.load('data/sim_paras.npy')
-model = SU2xSU2(*paras)
-model.load_data()
-
+# # run single simulation and store unthinned chain
+model_paras = {'N':32, 'a':1, 'ell':3, 'eps':1/3, 'beta':2}
+paras_calibrated = calibrate(model_paras)
+print('calibration completed!')
+sim_paras = {'M':2000, 'thin_freq':1, 'burnin_frac':0.1, 'store_data':False}
+model, paras = calibrate(paras_calibrated, sim_paras, production_run=True)
 unthinned_configs = model.configs
+print('Parameters used during production run: ',paras)
+
+# # or load stored simulation
+# paras = np.loadtxt('data/model_paras.txt')
+# model = SU2xSU2(*paras)
+# model.load_data()
+# unthinned_configs = model.configs
+
 
 for i,thin_freq in enumerate(thin_freqs):
     thinned_configs = unthinned_configs[::thin_freq]
     model.configs = thinned_configs
     model.M = thinned_configs.shape[0]
-    _,_, s_IAT[i], s_IAT_err[i] = model.action_per_site()
-    _,_, c_IAT[i], c_IAT_err[i] = model.specific_heat_per_site()
+    _,_, e_IAT[i], e_IAT_err[i] = model.internal_energy_density()
 
 
 fig = plt.figure(figsize=(8,6))
 
-plt.errorbar(thin_freqs, s_IAT, yerr=s_IAT_err, fmt='x', capsize=2, color='k', label='action per site')
-plt.plot(thin_freqs, s_IAT, color='k')
-plt.errorbar(thin_freqs, c_IAT, yerr=c_IAT_err, fmt='x', capsize=2, color='g', label='specific heat per site')
-plt.plot(thin_freqs, c_IAT, color='g')
+plt.errorbar(thin_freqs, e_IAT, yerr=e_IAT_err, fmt='x', capsize=2, color='k')
+plt.plot(thin_freqs, e_IAT, color='k')
 plt.xscale('log')
 plt.xlabel('thinning frequency')
 plt.ylabel(r'$\tau$')
-plt.legend(prop={'size': 12})
 
 plt.show()
 # fig.savefig('plots/thin_freq.pdf)
